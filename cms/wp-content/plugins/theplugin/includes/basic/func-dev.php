1a005e4cd99b2bae6cee4d41749ff7eb6d971aa4
yamerun <yarwizard@gmail.com>
2025-10-22 23:10:49 +0500

    ADD ThePlugin dev-tool `replace in db-files`

--- cms/wp-content/plugins/theplugin/includes/basic/func-dev.php
+++ cms/wp-content/plugins/theplugin/includes/basic/func-dev.php
@@ -1,5 +1,9 @@
 <?php
 
+defined('ABSPATH') || exit;
+
+add_filter('theplugin_active_counters', 'theplugin_is_deactive_counters', 20, 1);
+
 /**
  * Отключение/включение кодов метрики в `tp_counters_code_head_id` и `tp_counters_code_footer_id`
  */
@@ -85,4 +89,61 @@ function theplugin_is_deactive_counters($active)
 
 	return $active;
 }
-add_filter('theplugin_active_counters', 'theplugin_is_deactive_counters', 20, 1);
+
+/**
+ * Редактирование текстовых файлов БД заменой строчек с учётом сериализированных данных
+ *
+ * @param array $files
+ * @param array $replaces
+ * @param integer $limit
+ * @return array|bool
+ */
+function theplugin_replace_in_db($files = [], $replaces = [], $limit = 0)
+{
+	if (!$files || !is_array($files))
+		return false;
+
+	// Проверка существования файла БД
+	$db = $files[0];
+	if (!file_exists($db))
+		return false;
+
+	// Удаляем существующий файл импорта отредактированных данных БД
+	$filelog = $files[1];
+	if (file_exists($filelog))
+		unlink($filelog);
+
+	$fileObj	= new SplFileObject($db);
+	$logs		= [];
+
+	foreach ($fileObj as $k => $line) {
+		foreach ($replaces as $old => $new) {
+			// Формируем паттерн сериализированных строчек
+			$pattern = sprintf('#s:([0-9]{1,}):\\\"%s#iu', wp_slash($old));
+			preg_match_all($pattern, $line, $matches);
+			if (count($matches[0])) {
+
+				$logs[$k]['matches'] = $matches;
+
+				foreach ($matches[1] as $m => $count) {
+					$count		+= mb_strlen($new) - mb_strlen($old);	// Изменяем количество символов в сериализированных данных
+					$pattern	= sprintf('s:%d:\"%s', $count, wp_slash($new));
+					$line		= str_replace($matches[0][$m], $pattern, $line);
+
+					$logs[$k]['line'][$m] = $line;
+				}
+			}
+		}
+
+		$line = strtr($line, $replaces);
+
+		$fp = fopen($filelog, 'a');
+		fwrite($fp, $line);
+		fclose($fp);
+
+		if ($limit <= $k && $limit)
+			break;
+	}
+
+	return $logs;
+}
