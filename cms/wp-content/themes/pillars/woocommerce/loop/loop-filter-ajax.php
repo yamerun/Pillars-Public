ba0f953dd38ef18871b30214b819df09ac2ba5cf
yamerun <yarwizard@gmail.com>
2026-05-05 19:12:53 +0500

    UPD Theme WC template filter `category thumbs`

--- cms/wp-content/themes/pillars/woocommerce/loop/loop-filter-ajax.php
+++ cms/wp-content/themes/pillars/woocommerce/loop/loop-filter-ajax.php
@@ -12,18 +12,25 @@
 defined('ABSPATH') || exit;
 
 $params = pillars_wc_set_categories_tab_items($args);
-if ($params['items']) { ?>
+if ($params['items']) {
+	$has_thumbs = (pillars_wc_product_cat_has_filter_thumbnails() === 'yes') ? true : false;
+	$class = ($has_thumbs) ? ' --thumb' : '';
+?>
 	<div id="category-tabs" class="block">
-		<nav class="pillars-wc-product-tabs__nav" <?= $params['attrs'] ?>>
-			<ul class="pillars-wc-product-tabs__wrapper" role="tablist">
+		<nav class="pillars-wc-product-tabs__nav<?= $class ?>" <?= $params['attrs'] ?>>
+			<ul class="pillars-wc-product-tabs__wrapper<?= $class ?>" role="tablist">
 				<?php foreach ($params['items'] as $item) {
+					if ($has_thumbs) {
+						$item['class'][] = $class;
+					}
 					echo sprintf(
-						'<li class="%s" id="tab-%s" data-id="%s" role="tab" aria-controls="tab-%s"><a href="%s">%s</a></li>',
+						'<li class="%s" id="tab-%s" data-id="%s" role="tab" aria-controls="tab-%s"><a href="%s">%s%s</a></li>',
 						esc_attr(join(' ', $item['class'])),
 						esc_attr($item['data-id']),
 						esc_attr($item['data-id']),
 						esc_attr($item['data-id']),
 						esc_attr($item['href']),
+						($has_thumbs) ? wp_get_attachment_image(get_term_meta($item['id'], '_pillars_cat_product_image_id', true)) : '',
 						wp_kses_post($item['label'])
 					);
 				} ?>
