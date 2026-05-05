e1c83d8dfabb2220dcbd25a5966a1c9c143ec100
yamerun <yarwizard@gmail.com>
2026-05-05 18:09:33 +0500

    UPD Theme JS&SCSS btn `recall me`

--- html/sources/assets/js/general/scroll-up.js
+++ html/sources/assets/js/general/scroll-up.js
@@ -31,3 +31,74 @@ if (document.querySelector('.btn-up')) {
 
 	btnUp.addEventListener();
 }
+
+if (document.querySelector('.btn-recall-me')) {
+	const btnRecall = {
+		el: document.querySelector('.btn-recall-me'),
+		show() {
+			// удалим у кнопки класс btn-up_hide
+			this.el.classList.remove('btn-recall-me__hide');
+		},
+		hide() {
+			// добавим к кнопке класс btn-up_hide
+			this.el.classList.add('btn-recall-me__hide');
+		},
+		addEventListener() {
+			let btn = this.el;
+
+			/*
+			let btnDelay = 1000 * 60;
+			let btnActive = false;
+
+			function btnShow() {
+				if (!btnActive) {
+					if (btn.classList.contains('--active')) {
+						btn.classList.remove('--active');
+						setTimeout(btnShow, btnDelay);
+					} else {
+						btn.classList.add('--active');
+						btnDelay += 1000 * 60;
+						setTimeout(btnShow, 1000 * 6);
+					}
+				} else {
+					setTimeout(btnShow, btnDelay);
+				}
+			}
+
+			let btnTimer = setTimeout(btnShow, btnDelay);
+
+			// при наведения курсора .btn-recall-me
+			document.querySelector('.btn-recall-me').onmouseover = () => {
+			if (!this.el.classList.contains('--active')) {
+			this.el.classList.add('--active');
+			}
+
+			btnActive = true;
+			clearInterval(btnTimer);
+			}
+
+			// при отведении курсора .btn-recall-me
+			document.querySelector('.btn-recall-me').onmouseout = () => {
+			if (this.el.classList.contains('--active')) {
+			this.el.classList.remove('--active');
+			}
+
+			btnActive = false;
+			btnTimer = setTimeout(btnShow, btnDelay);
+			}
+			*/
+
+			setTimeout(function recallMe() {
+				if (btn.classList.contains('--animate')) {
+					btn.classList.remove('--animate');
+					setTimeout(recallMe, 1000 * 15);
+				} else {
+					btn.classList.add('--animate');
+					setTimeout(recallMe, 1000 * 3);
+				}
+			}, 1000 * 15);
+		}
+	}
+
+	btnRecall.addEventListener();
+}
